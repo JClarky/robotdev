fd01c090e371a9a5c55b4790476a942367101e4f
JClarky <jayden260100@gmail.com>
2020-11-02 10:22:29 +1300

    ROBOT BUILD

--- main/main_code/main.cpp
+++ main/main_code/main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <stdio.h>
 #include <hardware_interface.h>
 #include <simulation_interface.h>
 
-// Libraries
+// OpenCV libraries
+#include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace std;
+using namespace cv;
 
 bool maze;
 
 void mode_maze()
 {
+    Mat
     while(maze)
     {
-
+        cout << "gtfo";
+        
     }
 }
 
