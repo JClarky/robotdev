179318abd602b3b397804099b8976aa35a5c3a1d
JClarky <jayden260100@gmail.com>
2020-11-02 08:49:43 +1300

    ROBOT BUILD

--- main/main_code/main.cpp
+++ main/main_code/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <hardware_interface.h>
+#include <simulation_interface.h>
+
+// Libraries
+#include <opencv2/videoio.hpp>
 
 using namespace std;
 
-/*int main()
+bool maze;
+
+void mode_maze()
 {
-    
-}*/
+    while(maze)
+    {
+
+    }
+}
+
+int main()
+{
+    maze = true;
+    mode_maze();
+}
