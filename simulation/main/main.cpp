61188a7e589333c8ad1890b0c84e6ada36669208
Jayden Clark <jayden260100@gmail.com>
2020-11-04 12:09:12 +1300

    SIM BUILD

--- simulation/main/main.cpp
+++ simulation/main/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 using namespace cv;
 
 bool maze;
-int threshold_min = 25;
+int threshold_min = 50;
 int threshold_max = 255;
 int largest_area = 0;
 int largest_contour_index = 0;
@@ -34,77 +34,71 @@ void mode_maze()
     
     while(maze)
     {        
+       
+        cap.read(frame); // Reads the cameras frame
+        if(frame.empty())
+        {
+            cout << "lmao the frame is blank";
+            break;
+        }
+
+        Mat grey;
+        cvtColor(frame, grey, COLOR_BGR2GRAY);
+
+        Mat blur;
+        GaussianBlur(grey, blur, Size(9,9), 0);
+
+        Mat threshold_img;
+        threshold(blur, threshold_img, threshold_min, threshold_max, THRESH_BINARY_INV);
+
+        vector<vector<Point> > contours;
+        vector<Vec4i> hierarchy;
+        
+        Mat contoured = frame.clone();
+        findContours(threshold_img.clone(), contours, hierarchy, 1, CHAIN_APPROX_NONE);
+        
+        for( size_t i = 0; i< contours.size(); i++ ) // iterate through each contour.
+        {
+            double area = contourArea( contours[i] );  
+
+            if( area > largest_area )
+            {
+                largest_area = area;
+                largest_contour_index = i;              
+            }
+        }
+
         try
         {
-            cap.read(frame); // Reads the cameras frame
-            if(frame.empty())
-            {
-                cout << "lmao the frame is blank";
-                break;
-            }
-
-            Mat grey;
-            cvtColor(frame, grey, COLOR_BGR2GRAY);
-
-            Mat blur;
-            GaussianBlur(grey, blur, Size(9,9), 0);
-
-            Mat threshold_img;
-            threshold(blur, threshold_img, threshold_min, threshold_max, THRESH_BINARY_INV);
-
-            vector<vector<Point> > contours;
-            vector<Vec4i> hierarchy;
-            
-            Mat contoured = frame.clone();
-            findContours(threshold_img.clone(), contours, hierarchy, 1, CHAIN_APPROX_NONE);
-            
-            for( size_t i = 0; i< contours.size(); i++ ) // iterate through each contour.
-            {
-                double area = contourArea( contours[i] );  
-
-                if( area > largest_area )
-                {
-                    largest_area = area;
-                    largest_contour_index = i;              
-                }
-            }
-
-            try
-            {
-                drawContours(contoured, contours, largest_contour_index, Scalar( 0, 255, 0 ), 2);
-                vector<Moments> mu(contours.size());
-                mu[0] = moments(contours[largest_contour_index]);
-                float cx = mu[0].m10 / mu[0].m00;
-                cout << "cx: " << cx;
-                cout << "\n";
-            }
-            catch(const exception& e)
-            {
-                cerr << e.what() << '\n';
-            }      
-            
-
-            try
-            {
-                //imshow("GREY", grey);
-                imshow("BLUR", blur);
-                imshow("THRESHOLD", threshold_img);
-                imshow("CONTOURED", contoured);
-            }
-            catch(const exception& e)
-            {
-                cerr << e.what() << '\n';
-                cout << "imshow error";
-            }        
-            
-
-            if (waitKey(5) >= 0)
-                break;
+            drawContours(contoured, contours, largest_contour_index, Scalar( 0, 255, 0 ), 2);
+            vector<Moments> mu(contours.size());
+            mu[0] = moments(contours[largest_contour_index]);
+            float cx = mu[0].m10 / mu[0].m00;
+            cout << "cx: " << cx;
+            cout << "\n";
         }
         catch(const exception& e)
         {
             cerr << e.what() << '\n';
+        }      
+        
+
+        try
+        {
+            //imshow("GREY", grey);
+            imshow("BLUR", blur);
+            imshow("THRESHOLD", threshold_img);
+            imshow("CONTOURED", contoured);
         }
+        catch(const exception& e)
+        {
+            cerr << e.what() << '\n';
+            cout << "imshow error";
+        }        
+        
+
+        if (waitKey(5) >= 0)
+            break;
     }
     return; // Camera disabled 
 }
