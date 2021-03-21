88aa5857ae88bac07423d654e75aaebc12b0de9c
Jayden Clark <jayden260100@gmail.com>
2021-03-22 11:19:22 +1300

    SIM BUILD

--- simulation/main/main.cpp
+++ simulation/main/main.cpp
@@ -241,7 +241,7 @@ void follow()
         using namespace chrono; // nanoseconds, system_clock, seconds
 
         sleep_for(nanoseconds(10)); // wait 10000 nano seconds otherwise we refresh faster than frame rate
-        sleep_until(system_clock::now() + nanoseconds(100000)); // change to 10000
+        sleep_until(system_clock::now() + nanoseconds(10000)); // change to 10000
 
         out.update(out); // Update the struct classs with current sensor data
 
