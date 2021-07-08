e780cbb48fd2f48c925a528171ade2becd609c2c
Jayden Clark <jayden260100@gmail.com>
2021-07-08 14:18:19 +1200

    DEPLOY

--- main/main_code/_interface/hardware_interface.cpp
+++ main/main_code/_interface/hardware_interface.cpp
@@ -316,7 +316,10 @@ void move(float left, float right) // value from -100 to 100
 		cout << "\nCALIBRATIOPN\n";
 		gpioServo(RIGHT_MOTOR_PIN, (int)new_value_r);
 		gpioServo(LEFT_MOTOR_PIN, (int)new_value_l);
-		usleep(1000000); // sleep 0.1s
+		usleep(100000); // sleep 0.1s
+		gpioServo(RIGHT_MOTOR_PIN, CENTER_THROTTLE);
+		gpioServo(LEFT_MOTOR_PIN, CENTER_THROTTLE);
+		usleep(1000000); // sleep 1s
 	}
 
 	gpioServo(RIGHT_MOTOR_PIN, (int)r_value);
